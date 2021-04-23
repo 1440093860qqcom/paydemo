81ee096bceb1059d7984f69f5c26f735a68be4a1
1440093860qqcom <1440093860@qq.com>
2021-04-23 10:18:10 +0800

    1.0.0

--- app/src/main/java/com/example/paydemo/MainActivity.java
+++ app/src/main/java/com/example/paydemo/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.paylibrary.Pay;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Pay.add(1, 2);
     }
 }
